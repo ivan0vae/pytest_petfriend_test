566b3967f205f5daad1fb546e55a60f9b4609e37
ivan0vae <orewek@hotmail.com>
2022-03-14 22:25:45 +0100

    add get list of pets

--- api.py
+++ api.py
@@ -18,3 +18,15 @@ class PetFriends:
         except:
             result = res.text
         return status, result
+
+    def get_list_of_pets(self, auth_key, filter):
+        headers = {'auth_key': auth_key['key']}
+        filter = {'filter': filter}
+        res = requests.get(self.base_url+'/api/pets', headers=headers, params=filter)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return status, result
--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -9,3 +9,11 @@ def test_get_api_key_for_valid_user(email = valid_email, password = valid_passwo
     assert status == 200
     assert 'key' in result
 
+def test_get_all_pets_with_valid_key(filter=''):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.get_list_of_pets(auth_key, filter)
+    assert status == 200
+    assert len(result['pets']) > 0
+
+
+
